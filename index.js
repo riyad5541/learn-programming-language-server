1fe8ae5c9eab05afdc8ef6ef10665da0833c2e76
riyad5541 <abdullahalriyad5541@gmail.com>
2022-10-26 18:42:12 +0600

    setup dynamic data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categoties =  require('./data/courseCategories.json');
+const categoryDetails = require('./data/course-info.json');
 
 app.get('/',(req, res) =>{
     res.send('course api running');
@@ -14,6 +15,18 @@ app.get('/',(req, res) =>{
 
 app.get('/course-categories', (req, res) =>{
     res.send(categoties)
+});
+
+app.get('/category/:id',(req,res)=>{
+    const id = req.params.id;
+    const category_course = categoryDetails.filter(n =>n.category_id === id);
+    res.send(category_course);
+})
+
+app.get('/course-info/:id',(req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = categoryDetails.find(c => c._id === id);
+    res.send(selectedCourse);  
 })
 
 app.listen(port, ()=>{
